648b31733df55163c7566478e5308dc49e907cda
anastasiabujnovskaa <buynast@list.ru>
2022-12-29 17:17:38 +0300

    Практикум 17

--- mod_17/Practice_17.py
+++ mod_17/Practice_17.py
@@ -1,6 +1,6 @@
 # Подаем на вход последовательность чисел, преобразуем в список
-# array = [int(x) for x in input("Введите последовательность чисел через пробел:").split()]
-array = [10,8,3,5,6,66,77,123]
+array = [int(x) for x in input("Введите последовательность чисел через пробел:").split()]
+
 # Сортируем список по возрастанию элементов в нем методом сортировки
 def merge_sort(array):
     if len(array) < 2:
@@ -10,7 +10,6 @@ def merge_sort(array):
         left = merge_sort(array[:middle])
         right = merge_sort(array[middle:])
         return merge(left, right)
-
 
 def merge(left, right):
     result = []
@@ -34,11 +33,9 @@ def merge(left, right):
 
     return result
 
-
 # Выводим отсортированный список
 array_sort = merge_sort(array)
 print(array_sort)
-
 
 #  Запрашиваем у пользователя любое число и проверяем, что оно соответствует указанному в условии ввода данных
 while True:
@@ -48,24 +45,25 @@ while True:
     except ValueError:
         print('Вы ввели не число, попробуйте снова: ')
 
-# Реализуем функцию, в которой устанавливается номер позиции элемента, который меньше введенного пользователем числа, а следующий за ним больше или равен этому числу
+# Реализуем функцию, в которой устанавливается номер позиции элемента,
+# который меньше введенного пользователем числа, а следующий за ним больше или равен этому числу
 def binary_search(array_int, element, left, right):
     if max(array_int) < element:
-        return print('введенное число больше максимального элемента в списке')
-    if min(array_int) > element:
-        return print('введенное число меньше минимального элемента в списке')
+        return print('Введенное число больше максимального элемента в списке')
+    if min(array_int) >= element:
+        return print('Введенное число меньше или равно минимальному элементу в списке')
 
-    middle = (right + left) // 2  # находим середину
-    if array_int[middle] == element:
-        return middle  # возвращаем этот индекс
+    middle = ((right + left) // 2)   # находим середину и округлим в большую сторону при нечетном кол-ве
+    if array_int[middle] >= element and array_int[middle-1] < element:
+        return middle - 1   # возвращаем этот индекс
     elif element < array_int[middle]:  # если элемент меньше элемента в середине
         # рекурсивно ищем в левой половине
         return binary_search(array_int, element, left, middle - 1)
     else:  # иначе в правой
         return binary_search(array_int, element, middle + 1, right)
 
-element_pos = binary_search(array_sort, user_element, 0, len(array_sort)-1)
+element_pos = binary_search(array_sort, user_element, 0, len(array_sort))
 if element_pos is not None: # нельзя просто писать If element_pos. Там мы приводим переменную к Bool и None = 0
-    print('номер позиции элемента в отсортированном списке, который меньше введенного пользователем числа, а следующий за ним больше или равен этому числу:', element_pos)
+    print('Номер позиции элемента:', element_pos)
 
 
